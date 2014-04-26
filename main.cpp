aeb66de9b0e5cce441ff8a9657cfcc6a5d01c77c
czechtech <Email>
2014-04-25 20:24:10 -0400

    Main.cpp is now a full featured app all unto itself.  This now provides the functionality of the previous QSettingsWebEditor.
    Due to the security hole this provides, this is not meant to serve as much more than a very flexible demo.
    
    The code is not arranged well for this reason.

--- main.cpp
+++ main.cpp
@@ -7,16 +7,55 @@ using namespace std;
 
 QCoreApplication *a;
 
-void signalHandler(int signum) { cerr << "Terminating\n"; a->exit(0); }
+class PathParser: public QSettingsWebEditor // Must derive from QSettingsWebEditor to have access to m_org & m_app
+{
+    Q_OBJECT
+public slots:
+	void onRequest(QHttpRequest* req, QHttpResponse*) {
+		QString path = req->path();
+		if(path.count("/") > 1) {
+			QString org = path.section("/", 1, 1);
+			QString app = path.section("/", 2, 2);
+			if( !org.isEmpty() && !app.isEmpty() ) {
+				m_org = org;
+				m_app = app;
+				return;
+			}
+		}
+		// TODO: Provide links to all accessible settings pages?
+		m_org = "CzechTech"; m_app = "QSettingsWebEditor";
+	}
+	void onSettingsChanged(QStringList keys)
+	{
+		if(!keys.join(" ").contains("Port")) return;
+		QSettings s("CzechTech", "QSettingsWebEditor");
+		setPort(s.value("Port").toInt());
+	}
+public:
+	explicit PathParser() : QSettingsWebEditor(NULL, 8080) {
+		connect(&m_server, SIGNAL(newRequest(QHttpRequest*, QHttpResponse*)), this, SLOT(onRequest(QHttpRequest*, QHttpResponse*)));
+	}
+};
+// Public Slot:
+
+
+void signalHandler(int) { cerr << "Terminating\n"; a->exit(0); }
+
 
 int main(int argc, char *argv[])
 {
   a = new QCoreApplication(argc, argv);
   signal(SIGINT,  signalHandler); signal(SIGTERM, signalHandler);
 
-  QSettingsWebEditor qswe;
+  QCoreApplication::setOrganizationName("CzechTech");
+  QCoreApplication::setApplicationName("QSettingsWebEditor");
+
+  QSharedSettings s;
+  PathParser qswe;
+
+  QObject::connect(&s, SIGNAL(settingsChanged(QStringList)), &qswe, SLOT(onSettingsChanged(QStringList)));
 
   return a->exec();
 }
 
-//#include "test.moc" //http://stackoverflow.com/questions/5854626/qt-signals-and-slots-error-undefined-reference-to-vtable-for
+#include "main.moc" //http://stackoverflow.com/questions/5854626/qt-signals-and-slots-error-undefined-reference-to-vtable-for
